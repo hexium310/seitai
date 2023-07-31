95a4439235125c6b45d9cc0cad602b80bf7cd8c8
Hexin <hexium310@gmail.com>
2023-08-01 02:53:22 +0900

    Add /dictionary help

--- seitai/src/commands/help.rs
+++ seitai/src/commands/help.rs
@@ -24,6 +24,22 @@ pub async fn run(context: &Context, interaction: &CommandInteraction) -> Result<
                         .title("/leave")
                         .description("ボイスチャンネルから切断します。"),
                 ),
+                "dictionary" => Some(
+                    CreateEmbed::new()
+                        .title("/dictionary")
+                        .description("辞書関連のコマンドです。")
+                        .fields([
+                            (
+                                "add",
+                                &format!(
+                                    "{}\n{}",
+                                    "単語を辞書に追加します。任意で指定できる`音が下がる位置`については次のリンクを参照してください。",
+                                    "https://tdmelodic.readthedocs.io/ja/latest/pages/introduction.html#representation-of-accent-nuclei-by-digits"
+                                ),
+                                false
+                            ),
+                        ]),
+                ),
                 _ => None,
             },
             _ => None,
@@ -34,8 +50,19 @@ pub async fn run(context: &Context, interaction: &CommandInteraction) -> Result<
         embeds.push(
             CreateEmbed::new()
                 .title("help")
-                .field("/join", "ボイスチャンネルに接続します。", true)
-                .field("/leave", "ボイスチャンネルから切断します。", false),
+                .fields([
+                    ("/join", "ボイスチャンネルに接続します。", true),
+                    ("/leave", "ボイスチャンネルから切断します。", true),
+                    (
+                        "/dictionary add",
+                        &format!(
+                            "{}\n{}",
+                            "単語を辞書に追加します。任意で指定できる`音が下がる位置`については次のリンクを参照してください。",
+                            "https://tdmelodic.readthedocs.io/ja/latest/pages/introduction.html#representation-of-accent-nuclei-by-digits"
+                        ),
+                        false,
+                    ),
+                ]),
         );
     }
 
@@ -47,7 +74,8 @@ pub async fn run(context: &Context, interaction: &CommandInteraction) -> Result<
 pub fn register() -> CreateCommand {
     let channels = CreateCommandOption::new(CommandOptionType::String, "command", "chose command")
         .add_string_choice("join", "join")
-        .add_string_choice("leave", "leave");
+        .add_string_choice("leave", "leave")
+        .add_string_choice("dictionary", "dictionary");
 
     CreateCommand::new("help")
         .description("Specific command to show help about")
