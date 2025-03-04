4d487759c73e8b6f28095f972c8abf375f50347b
Hexin <hexium310@gmail.com>
2025-03-04 17:30:10 +0900

    Remove CREATE SCHEMA from migration

--- crates/database/src/migrations/v1_users_and_speakers.rs
+++ crates/database/src/migrations/v1_users_and_speakers.rs
@@ -5,41 +5,10 @@ use sqlx_migrator::{migration::Migration, operation::Operation, vec_box};
 
 use crate::{speaker::DatabaseSpeaker, user::DatabaseUser};
 
-pub(crate) struct CreateSchemaOperation;
 pub(crate) struct CreateTableOperation;
 pub(crate) struct CreateIndexOperation;
 
 pub(crate) struct V1Migration;
-
-impl Operation<Postgres> for CreateSchemaOperation {
-    fn up<'a, 'b, 'async_trait>(&'a self, connection: &'b mut PgConnection) -> BoxFuture<'async_trait, Result<(), sqlx_migrator::error::Error>>
-    where
-        Self: 'async_trait,
-        'a: 'async_trait,
-        'b: 'async_trait,
-    {
-        Box::pin(async {
-            sqlx::query("CREATE SCHEMA IF NOT EXISTS seitai AUTHORIZATION seitai").execute(&mut *connection).await?;
-
-            Ok(())
-        })
-    }
-
-    fn down<'a, 'b, 'async_trait>(&'a self, _connection: &'b mut PgConnection) -> BoxFuture<'async_trait, Result<(), sqlx_migrator::error::Error>>
-    where
-        Self: 'async_trait,
-        'a: 'async_trait,
-        'b: 'async_trait,
-    {
-        Box::pin(async {
-            // The `seitai` schema is used as the _sqlx_migrator_migrations schema.
-            // If dropping the `seitai` schema with `CASCADE`, _sqlx_migrator_migrations is also dropped, so reverting migration fails by sqlx_migrator.
-            //sqlx::query("DROP SCHEMA seitai").execute(&mut *connection).await?;
-
-            Ok(())
-        })
-    }
-}
 
 impl Operation<Postgres> for CreateTableOperation {
     fn up<'a, 'b, 'async_trait>(&'a self, connection: &'b mut PgConnection) -> BoxFuture<'async_trait, Result<(), sqlx_migrator::error::Error>>
@@ -165,7 +134,6 @@ impl Migration<Postgres> for V1Migration {
 
     fn operations(&self) -> Vec<Box<dyn Operation<Postgres>>> {
         vec_box![
-            CreateSchemaOperation,
             CreateTableOperation,
             CreateIndexOperation,
         ]
