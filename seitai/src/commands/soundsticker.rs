b3d7a385f3631d5e5715f5f2e85b3ca8eb660ac4
Hexin <hexium310@gmail.com>
2025-03-05 07:57:44 +0900

    Filter items case-insensitive

--- seitai/src/commands/soundsticker.rs
+++ seitai/src/commands/soundsticker.rs
@@ -191,7 +191,7 @@ pub(crate) async fn autocomplete(context: &Context, interaction: &CommandInterac
 fn sticker_autocomplete(stickers: &[Sticker], value: &str) -> CreateInteractionResponse {
     let choices = stickers
         .iter()
-        .filter(|&sticker| sticker.name.contains(value))
+        .filter(|&sticker| sticker.name.to_lowercase().contains(&value.to_lowercase()))
         .map(|sticker| AutocompleteChoice::new(sticker.name.clone(), sticker.id.to_string()))
         .take(25)
         .collect::<Vec<_>>();
@@ -203,7 +203,7 @@ fn soundboard_autocomplete(soundboard: Soundboard, value: &str) -> CreateInterac
     let choices = soundboard
         .items
         .iter()
-        .filter(|&sound| sound.name.contains(value))
+        .filter(|&sound| sound.name.to_lowercase().contains(&value.to_lowercase()))
         .map(|sound| AutocompleteChoice::new(sound.name.clone(), sound.sound_id.get().to_string()))
         .take(25)
         .collect::<Vec<_>>();
